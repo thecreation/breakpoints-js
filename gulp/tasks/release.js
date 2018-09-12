2d850711c27711214e13b0fd89ef2edc76c8f2ab
kaptinlin <kaptinlin@gmail.com>
2018-09-12 15:44:42 +0800

    Release 1.0.6

--- gulp/tasks/release.js
+++ gulp/tasks/release.js
@@ -13,6 +13,10 @@ export default function release() {
   options.force = argv.force || false;
   options['dry-run'] = argv['dry-run'] || false;
   options.requireCleanWorkingDir = false;
+  options.tagName = "v%s";
+  options.tagAnnotation = "Release v%s";
+  options.buildCommand = "gulp build";
+  options.pkgFiles = ["package.json", "bower.json"]
   config.setEnv('production');
 
   return function(done) {
